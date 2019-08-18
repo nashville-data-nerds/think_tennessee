5d6aeeeca5e96eb2822d744ec31f5f19dd0c745b
alex.antonison <alex.antonison@gmail.com>
2019-08-18 08:05:51 -0500

    Updated setup script

--- development/setup_conda_env.sh
+++ development/setup_conda_env.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-conda activate base
+########
+# before running script, be in your base env 
+# you can activate base with the following command:
+# conda activate base
+######
 conda remove --name think_tn --all
 conda env create -f ../environment.yml
