75d319e838394dfc62ed916032aa4a7f4700e1fb
alex.antonison <alex.antonison@gmail.com>
2019-08-18 07:59:33 -0500

    Add libraries and script for creating env file

---
+++ development/create_conda_env_file.sh
@@ -0,0 +1,11 @@
+# Create environment file
+conda env export --no-builds > ../environment.yml
+
+# Remove bad packages
+sed -i ".bak" '/llvm-openmp/d' ../environment.yml
+sed -i ".bak" '/libcxxabi/d' ../environment.yml
+sed -i ".bak" '/appnope/d' ../environment.yml
+sed -i ".bak" '/libgfortran/d' ../environment.yml
+sed -i ".bak" '/libcxx/d' ../environment.yml
+
+rm ../environment.yml.bak
